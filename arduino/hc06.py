9ade77553a004c9803a0c8784cba607e715eb249
yeonshin <38985716+yondini@users.noreply.github.com>
2018-12-04 16:53:07 +0900

    Update hc06.py

--- arduino/hc06.py
+++ arduino/hc06.py
@@ -3,5 +3,5 @@ import serial
 bluetoothSerial = serial.Serial("/dev/rfcomm0", baudrate=9600)
 def fire():
 	while 1:
-	    variable = (int)(bluetoothSerial.readline()
+	    variable = (int)(bluetoothSerial.readline())
 	    return variable
